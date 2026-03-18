3bc4991b6c3c4019f546ed1a327dc589855201f9
Luis Felipe Marinho Revorêdo da Paz <luis.revoredo@ufrpe.br>
2026-03-18 01:15:18 -0300

    cinemachineCamera

--- Assets/Scripts/Player/player_moviment.cs
+++ Assets/Scripts/Player/player_moviment.cs
@@ -24,32 +24,41 @@ public class player_moviment : MonoBehaviour
 {
     
 }
-
-    // Update is called once per frame
     void FixedUpdate()
-
     {   
-      
+        //Caso o jogador comece a atacar e estiver se movendo ele deve parar de se mover e usar o ataque
         if(isAttacking == true && isMoving == true)
         {
             rb.linearVelocity = new Vector2(moveInput.x,0) * 0;
             anim.SetBool("moving",false);
         }
-        else if(isJumping == true && Mathf.Abs(rb.linearVelocity.y) < 0.01f)
+        // caso o jogador não tenha clicado para pular mas por algum motivo esteja em queda
+        else if(isJumping == false && Mathf.Abs(rb.linearVelocity.y) > 3.2f && Mathf.Abs(rb.linearVelocity.y)  < 3.4f)
+        {
+            isJumping = true;
+            anim.SetBool("jumping",true);
+            anim.SetBool("isIdle",false);
+            anim.SetBool("moving",false);
+            rb.linearVelocity = new Vector2(moveInput.x * velocity * 0.6f,jumpForce);
+        }
+        // caso o jogador esteja pulando mas já tenha tocado o chão
+        else if(isJumping == true && Mathf.Abs(rb.linearVelocity.y) < 0.2f)
         {
             isJumping = false;
             anim.SetBool("jumping", false);
             anim.SetBool("isIdle",true);
         }
+        //caso o usuário ainda esteja no meio do salto a gente reduz a força e impulso dele para iniciar a desaceleração
         else if(isJumping == true || jumpForce != 0)
         {
-            rb.linearVelocity = new Vector2(moveInput.x * velocity,jumpForce);
+            rb.linearVelocity = new Vector2(moveInput.x * velocity * 0.8f,jumpForce);
             if(jumpForce > 0){
-                jumpForce -= 1;
+                jumpForce -= 0.5f;
             }
             anim.SetBool("moving",false);
             anim.SetBool("isIdle",false);
         }
+        //caso contrário o jogador estará em movimento ou parado
         else
         {
             rb.linearVelocity = new Vector2(moveInput.x * velocity,jumpForce) ;
@@ -67,7 +76,6 @@ public class player_moviment : MonoBehaviour
                      isMoving = false;
                      anim.SetBool("isIdle",true);
                 }
-           
         }
         }
            
@@ -107,7 +115,6 @@ public class player_moviment : MonoBehaviour
 
     public void OnJump (InputValue value)
     {
-
          if(isJumping != true && Mathf.Abs(rb.linearVelocity.y) < 0.01f)
         {
             anim.SetBool("jumping", true);
@@ -115,12 +122,10 @@ public class player_moviment : MonoBehaviour
             anim.SetBool("moving",false);
             isMoving = false;
             isJumping = true;
-            jumpForce = 12;
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x,jumpForce);
+            jumpForce = 11;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x* 0.7f,jumpForce);
             
         }
-       
-
     }
 
 
@@ -144,6 +149,6 @@ public class player_moviment : MonoBehaviour
             facingDirection = 1;
         }
 
-        transform.localScale = new Vector3(facingDirection,1,1);
+        transform.localScale = new Vector3(facingDirection * 1.6f,1.6f,1);
     }
 }
