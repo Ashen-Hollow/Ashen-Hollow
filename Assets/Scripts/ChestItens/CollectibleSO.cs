bfa3684dfc9d6200a5a8f4c107612fb53f24c364
unknown <viniciusgabrielss10@gmail.com>
2026-05-24 04:19:12 -0300

    feat: implementação do bau e itens

---
+++ Assets/Scripts/ChestItens/CollectibleSO.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public abstract class CollectibleSO : ScriptableObject
+{
+    public string itemName;
+    public Sprite icon;
+    public Sprite itemSprite;
+
+    public abstract void Collect(Player player);
+
+}
