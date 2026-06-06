782a2a10dc1273fd21cdb10403a51d87dd323599
unknown <viniciusgabrielss10@gmail.com>
2026-06-06 19:39:48 -0300

    fix:ajustes

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -48,4 +48,13 @@ public class InventoryManager : MonoBehaviour
         }
     }
 
+    public void DeselecteAllSlots()
+    {
+        for (int i = 0; i < itemSlot.Length; i++)
+        {
+            itemSlot[i].selectedShader.SetActive(false);
+            itemSlot[i].thisItemSelected = false;
+        }
+    }
+
 }
--- Assets/Scripts/Inventory/ItemSlot.cs
+++ Assets/Scripts/Inventory/ItemSlot.cs
@@ -3,8 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class ItemSlot : MonoBehaviour
+public class ItemSlot : MonoBehaviour , IPointerClickHandler
 {
     public string itemName;
     public int quantity;
@@ -14,6 +15,17 @@ public class ItemSlot : MonoBehaviour
     [SerializeField] private TMP_Text quantityText;
     [SerializeField] private Image itemImage;
 
+
+    public GameObject selectedShader;
+    public bool thisItemSelected;
+
+    private InventoryManager inventoryManager;
+
+
+    public void Start()
+    {
+       inventoryManager = GameObject.Find("InventoryCanvas").GetComponent<InventoryManager>(); 
+    }
     public void AddItem(string itemName, int quantity, Sprite itemSprite)
     {
         this.itemName = itemName;
@@ -29,5 +41,29 @@ public class ItemSlot : MonoBehaviour
 
         // LIGA O COMPONENTE DE IMAGEM
         itemImage.enabled = true;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            OnLeftClick();
+        }
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightClick();
+        }
+
+    }
+    public void OnLeftClick() 
+    { 
+        inventoryManager.DeselecteAllSlots();
+       selectedShader.SetActive(true);
+        thisItemSelected = true;
+    }
+
+    public void OnRightClick() 
+    {
+
     }
 }
